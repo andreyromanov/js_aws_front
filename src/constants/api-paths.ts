e3cb0088a9cce83026e263e829b71a90d97ae024
Andrii Romanov <andreygorogogo@gmail.com>
2023-03-15 12:18:11 +0200

    Task 3: updated links to API (#4)
    * Added links to S3 and CloudFront. Changed app title.
    
    * Deployed app to AWS with Serverless
    
    * Updated node version
    
    * Changed bucket AccessControl to private
    
    * task-3: retrieve products from api
    
    * changed url to updated API
    
    ---------
    
    Co-authored-by: Andrii Romanov <andrii.romanov@dat.com>

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,8 @@
 export const API_PATHS = {
-	product: 'https://htplvd3xti.execute-api.us-east-1.amazonaws.com/dev',
+	product: 'https://z92nx2nsl2.execute-api.eu-west-2.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
+
