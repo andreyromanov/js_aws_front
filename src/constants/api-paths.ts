6b8c90609404c3fd7277da42c2ad3dc8db81164f
Andrii Romanov <andrii.romanov@dat.com>
2023-04-10 22:48:21 +0300

    task-9: Integrated with EBS infrastructure

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://z92nx2nsl2.execute-api.eu-west-2.amazonaws.com/dev',
-	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	order: 'http://cart-api.eu-west-2.elasticbeanstalk.com/api',
 	import: 'https://yxpwlrg3pi.execute-api.eu-west-2.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	cart: 'http://cart-api.eu-west-2.elasticbeanstalk.com/api',
 };
